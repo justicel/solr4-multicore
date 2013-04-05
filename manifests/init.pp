122a24a24d7456759d931d65630a9ee5dd11028c
Justice London <jlondon@syrussystems.com>
2013-04-05 10:34:24 -0700

    Added refreshonly to init.

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,7 +6,8 @@ class solr (
 
 ){
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command     => '/usr/bin/apt-get update'
+    refreshonly => true,
   }
 
   package { "tomcat6":
