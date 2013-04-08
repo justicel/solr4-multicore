860b48e18366a4878748274da104694559f78c13
Justice London <jlondon@syrussystems.com>
2013-04-08 08:27:46 -0700

    Restart on changes.

--- manifests/core.pp
+++ manifests/core.pp
@@ -12,45 +12,52 @@ define solr::core(
     owner   => "${solr::params::tomcat_user}",
     group   => "${solr::params::tomcat_group}",
     mode    => '0755',
+    notify  => Service['tomcat6'],
   }
 
   exec { "cp-conf-data-${name}":
     command => "cp -R solr-${solr::params::solr_version}/example/multicore/core0/* ${solr_home}/${name}/",
-    cwd => "/home/vagrant/",
-    path => ["/usr/bin", "/usr/sbin/", "/bin"],
+    cwd     => "/home/vagrant/",
+    path    => ["/usr/bin", "/usr/sbin/", "/bin"],
     require => File["${solr_home}/${name}"],
+    notify  => Service['tomcat6'], 
   }
 
   file {
     "${name}-solrconfig.xml":
       ensure  => file,
-      path => "${solr_home}/${name}/conf/solrconfig.xml",
+      path    => "${solr_home}/${name}/conf/solrconfig.xml",
       content => template('solr/solrconfig.xml.erb'),
-      require => Exec["cp-conf-data-${name}"];
+      require => Exec["cp-conf-data-${name}"],
+      notify  => Service['tomcat6'];
 
     "${name}-schema.xml":
       ensure  => file,
-      path => "${solr_home}/${name}/conf/schema.xml",
+      path    => "${solr_home}/${name}/conf/schema.xml",
       content => template('solr/schema.xml.erb'),
-      require => Exec["cp-conf-data-${name}"];
+      require => Exec["cp-conf-data-${name}"],
+      notify  => Service['tomcat6'];
 
     "${name}-stopwords.txt":
       ensure  => file,
-      path => "${solr_home}/${name}/conf/stopwords.txt",
+      path    => "${solr_home}/${name}/conf/stopwords.txt",
       content => template('solr/stopwords.txt.erb'),
-      require => Exec["cp-conf-data-${name}"];
+      require => Exec["cp-conf-data-${name}"],
+      notify  => Service['tomcat6'];
 
     "${name}-synonyms.txt":
       ensure  => file,
-      path => "${solr_home}/${name}/conf/synonyms.txt",
+      path    => "${solr_home}/${name}/conf/synonyms.txt",
       content => template('solr/synonyms.txt.erb'),
-      require => Exec["cp-conf-data-${name}"];
+      require => Exec["cp-conf-data-${name}"],
+      notify  => Service['tomcat6'];
 
     "${name}-protwords.txt":
       ensure  => file,
-      path => "${solr_home}/${name}/conf/protwords.txt",
+      path    => "${solr_home}/${name}/conf/protwords.txt",
       content => template('solr/protwords.txt.erb'),
-      require => Exec["cp-conf-data-${name}"];
+      require => Exec["cp-conf-data-${name}"],
+      notify  => Service['tomcat6'];
   }
 
 }
