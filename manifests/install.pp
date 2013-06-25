4791fd0fe25dc4a799a1264434b9320afead274a
Justice London <jlondon@syrussystems.com>
2013-06-25 16:22:31 -0400

    Updated to copy log4j config to main classpath.

--- manifests/install.pp
+++ manifests/install.pp
@@ -21,19 +21,13 @@ class solr::install($solr_home,$version,$install_source) inherits solr::params {
     owner   => "${solr::params::tomcat_user}",
     group   => "${solr::params::tomcat_group}",
     mode    => '0755',
-  }
-  file { "${solr_home}/classes":
-    ensure   => directory,
-    owner    => "${solr::params::tomcat_user}",
-    group    => "${solr::params::tomcat_group}",
-    mode     => '0755',
-    require  => File["${solr_home}"],
+    recurse => true,
   }
 
   exec { "solr-install":
     command => "cp -R ${version}/example/solr/bin ${solr_home}/bin | cp ${version}/example/multicore/zoo.cfg ${solr_home}/ | cp -R ${version}/example/webapps/solr.war ${solr_home}/",
-    cwd => "/var/tmp/",
-    path => ["/usr/bin", "/usr/sbin/", "/bin"],
+    cwd     => "/var/tmp/",
+    path    => ["/usr/bin", "/usr/sbin/", "/bin"],
     require => Exec["solr-inflate"]
   }
 
@@ -47,7 +41,7 @@ class solr::install($solr_home,$version,$install_source) inherits solr::params {
 
   #Add log4j properties file
   exec { "log4j-properties":
-    command => "cp ${version}/example/cloud-scripts/log4j.properties ${solr_home}/classes",
+    command => "cp ${version}/example/cloud-scripts/log4j.properties /usr/share/tomcat6/lib",
     cwd     => '/var/tmp',
     path    => ['/usr/bin', '/usr/sbin', '/bin'],
     require => File["${solr_home}/classes"],
